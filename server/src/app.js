1014a25df54fc0bbc7d6cabe5be945fdd4ffd5d8
ferntyp <70442698+ferntyp@users.noreply.github.com>
2020-09-01 18:17:37 +0700

    chapter5

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,10 @@ let express = require('express')
 let bodyParser = require('body-parser')
 
 const app = express()
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+require('./routes')(app)
 app.get('/status', function (req, res ){
  res.send('Hello nodejs server')
 })
@@ -13,32 +15,8 @@ app.get('/hello/:person', function (req,res) {
  res.send('sey hello with ' + req.params.person)
 })
 
-// get user by id
-app.get('/user/:userId', function (req, res) {
- res.send('ดูข้อมูลผู้ใช้งาน: ' + req.params.userId)
-})
-
-// get all user
-app.get('/users', function (req, res) {
- res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
-})
-
-// create user
-app.post('/user/', function (req, res) {
- res.send('ทําการสร้างผู้ใช้งาน: ' + JSON.stringify(req.body))
-})
-
-// edit user
-app.put('/user/:userId', function (req, res) {
- res.send('ทำการแก้ไขผู้ใช้งาน: ' + req.params.userId + ' : ' +
-JSON.stringify(req.body))
-})
-
-// delete user
-app.delete('/user/:userId', function (req, res) {
- res.send('ทําการลบผู้ใช้งาน: ' + req.params.userId + ' : ' +
- JSON.stringify(req.body))
-63
+app.post('/hello', function (req, res) {
+ res.send('OK you post - ' + req.body.name)
 })
 
 let port = 8081
